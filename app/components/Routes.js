eacfcbe18af89ed96ad4c56b3c076fb463b8d3d9
Erik Mudrak <ejmudrak17@transy.edu>
2017-02-21 22:34:40 -0500

    React commit

---
+++ app/components/Routes.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Route, IndexRoute } from 'react-router';
+
+import App from './App';
+import Home from '../pages/home/page';
+
+export default (
+  <Route path="/" component={App}>
+    <IndexRoute component={Home} />
+    // Add more routes here
+  </Route>
+);
