4b2f56e5b80a135af77aa01cbff31fb65ae8256a
Erik Mudrak <ejmudrak17@transy.edu>
2017-04-29 04:18:22 -0400

    Fixes

--- app/pages/home/page.js
+++ app/pages/home/page.js
@@ -8,10 +8,10 @@ import React from 'react';
 import styles from "./style.css";
 import MediaQuery from 'react-responsive';
 
-import CodeIcon from '../../assets/code-blue.png';
-import DesignIcon from '../../assets/design-blue.png';
-import CodeSnippetIcon from '../../assets/code-snippet.png';
-import Portrait from '../../assets/portrait.jpg';
+// import CodeIcon from '../../assets/code-blue.png';
+// import DesignIcon from '../../assets/design-blue.png';
+// import CodeSnippetIcon from '../../assets/code-snippet.png';
+// import Portrait from '../../assets/portrait.jpg';
 
 const aboutText = "Hello, I'm Erik Mudrak, and I am drawn to user interface design, front end development, and project management. I strive to meld my skills to solve problems with tech in fresh places and in fresh ways."; 
 const secondaryText = "I'm looking for a team to build alongside";
@@ -37,14 +37,14 @@ class Portfolio extends React.Component {
         <div className={styles.portfolioHeading}>Portfolio</div>
         <a className={styles.iconWrapper} href="http://www.github.com/ejmudrak" target="_blank">
           <div className={styles.circle} >
-            <img className={styles.codeIcon} src={CodeIcon} />
+            <img className={styles.codeIcon} src={require('../../assets/code-blue.png')} />
           </div>
           <div className={styles.label}>Code</div>
         </a>
 
         <a className={styles.iconWrapper} href="http://www.behance.net/ejmudrak" target="_blank">
           <div className={[styles.circle, styles.designCircle].join(' ')} >
-            <img className={styles.designIcon} src={DesignIcon} />
+            <img className={styles.designIcon} src={require('../../assets/design-blue.png')} />
           </div>
           <div className={styles.label}>Design</div>
         </a>
@@ -61,7 +61,7 @@ class About extends React.Component {
        <div className={styles.aboutHeading}>ERIK MUDRAK</div>
        <div className={styles.line}></div>
        <div className={styles.topWrapper}>
-         <div className={styles.portraitWrapper}><img className={styles.portrait} src={Portrait} /></div>
+         <div className={styles.portraitWrapper}><img className={styles.portrait} src={require('../../assets/portrait.jpg')} /></div>
          <p className={styles.aboutText}>{ aboutText }</p>
        </div>
        <MediaQuery query='(min-device-width: 1400px)'>
@@ -80,7 +80,7 @@ class CodeSnippet extends React.Component {
   render () {
     return (
       <div>
-        <img className={styles.codeSnippet} src={CodeSnippetIcon} />
+        <img className={styles.codeSnippet} src={require('../../assets/code-snippet.png')} />
       </div>
     );
   }
