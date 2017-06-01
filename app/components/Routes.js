4a6fb9a497b3db60d5432b481668776dd2372844
Erik Mudrak <ejmudrak17@transy.edu>
2017-06-01 01:27:23 -0400

    Site redesign initial commit

--- app/components/Routes.js
+++ app/components/Routes.js
@@ -3,12 +3,10 @@ import { Route, IndexRoute } from 'react-router';
 
 import App from './App';
 import Home from '../pages/home/page';
-import Test from '../pages/test/page';
 
 export default (
   <Route path="/" component={App}>
     <IndexRoute component={Home} />
     <Route path="home" component={Home} />
-    <Route path="test" component={Test} />
   </Route>
 );
--- app/pages/home/page.js
+++ app/pages/home/page.js
@@ -9,10 +9,14 @@ import styles from "./style.css";
 import { browserHistory } from 'react-router';
 import MediaQuery from 'react-responsive';
 
+import FloatingActionButton from 'material-ui/FloatingActionButton';
+
 import CodeIcon from './code-blue.png';
 import DesignIcon from './design-blue.png';
 import CodeSnippetIcon from './code-snippet.png';
 import Portrait from './portrait.jpg';
+import Name from './wordmark2.png';
+import Computer from './computer.png';
 
 const aboutText = "Hello, I'm Erik Mudrak, and I am drawn to user interface design, front end development, and project management. I strive to meld my skills to solve problems with tech in fresh places and in fresh ways."; 
 const secondaryText = "I'm looking for a team to build alongside";
@@ -21,9 +25,12 @@ export default class Home extends React.Component {
     render() {
         return (
             <div className={styles.container}>
-              <About /> 
-              <Portfolio />
-              <div className={styles.backgroundTriangle}></div>
+              <Background />
+              <div className={styles.nameWrapper}>
+                <img className={styles.name} src={Name} />
+              </div>
+              <Icons />
+              <About />
             </div>
         );
     }
@@ -39,13 +46,13 @@ class Portfolio extends React.Component {
         <a className={styles.iconWrapper} href="http://www.github.com/ejmudrak" target="_blank">
           <div className={styles.circle} >
             <img className={styles.codeIcon} src={CodeIcon} />
-          </div>
+          </div>
           <div className={styles.label}>Code</div>
-        </a>
+        </a>
        <MediaQuery query='(max-device-width: 750px)'>
          <div className={styles.divider}></div> 
       </MediaQuery>
-        <a className={styles.iconWrapper} href="http://www.behance.net/ejmudrak" target="_blank">
+        <a className={styles.iconWrapper} href="http://www.behance.net/ejmudrak" target="_blank">
           <div className={[styles.circle, styles.designCircle].join(' ')} >
             <img className={styles.designIcon} src={DesignIcon} />
           </div>
@@ -61,29 +68,10 @@ class About extends React.Component {
   render () {
     return (
       <div className={styles.aboutContainer}>
-       <div className={styles.aboutHeading}>ERIK MUDRAK</div>
-       <div className={styles.line}></div>
-       <div className={styles.topWrapper}>
+       <div className={styles.aboutWrapper}>
          <div className={styles.portraitWrapper}><img className={styles.portrait} src={Portrait} /></div>
          <p className={styles.aboutText}>{ aboutText }</p>
        </div>
-       <MediaQuery query='(min-device-width: 1400px)'>
-         <div className={styles.bottomWrapper}>
-          <Blocks />
-          <CodeSnippet /> 
-        </div> 
-      </MediaQuery>
-      </div>
-    );
-  }
-}
-
-class CodeSnippet extends React.Component {
-
-  render () {
-    return (
-      <div>
-        <img className={styles.codeSnippet} src={CodeSnippetIcon} />
       </div>
     );
   }
@@ -104,4 +92,43 @@ class Blocks extends React.Component {
     );
   }
 }
+
+class CodeSnippet extends React.Component {
+
+  render () {
+    return (
+      <div>
+        <img className={styles.codeSnippet} src={CodeSnippetIcon} />
+      </div>
+    );
+  }
+}
+
+class Background extends React.Component {
+
+  render () {
+    return (
+      <div>
+       <div className={styles.backgroundBack}></div>
+       <div className={styles.backgroundMid}></div>
+       <div className={styles.backgroundFront}></div>
+      </div>
+    );
+  }
+}
+
+class Icons extends React.Component {
+
+  render () {
+    return (
+      <div className={styles.buttonContainer}>
+        <FloatingActionButton className={styles.button} backgroundColor="#4A5DEC" />
+        <FloatingActionButton className={styles.button} backgroundColor="#d95779"/>
+        <FloatingActionButton className={styles.button} backgroundColor="#fff"/>
+      </div>
+    );
+  }
+}
+
+
 
